c329a47ee0df148515640e89dd9b15030d330af0
hosty <{ID}+{username}@users.noreply.github.com>
2023-05-26 00:57:38 -0400

    working changes

--- shadow.ts
+++ shadow.ts
@@ -1,33 +1,25 @@
-import anchor from "@project-serum/anchor";
-import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
-import { ShdwDrive, ShadowFile } from "@shadow-drive/sdk";
-import * as fs from "fs";
-import key from "./shQBVFZbngBA2zeLoKr9AgpyNvwMFTJo36MwavMTQcV.json";
+const anchor = require("@project-serum/anchor");
+const { Connection, clusterApiUrl, Keypair } = require("@solana/web3.js");
+const { ShdwDrive, ShadowFile } = require("@shadow-drive/sdk");
+const key =  require('./NP3SYi4apdYNZC55jdePch2k3us3oWLdyYr3dKSsptq.json');
+const fs = require('fs');
 
 async function main() {
     let secretKey = Uint8Array.from(key);
     let keypair = Keypair.fromSecretKey(secretKey);
     const connection = new Connection(
-        clusterApiUrl("mainnet-beta"),
-        "confirmed"
-    );
-    const wallet = new anchor.Wallet(keypair);
-    const drive = await new ShdwDrive(connection, wallet).init();
-    const newAcct = await drive.createStorageAccount("myDemoBucket", "10MB", "v2");
-    console.log(newAcct);   
-    const fileBuff = fs.readFileSync("./mytext.txt");
-    const acctPubKey = new anchor.web3.PublicKey(
-    "0xF5f6eBA8498120B8408C3ecDa1898a1Df2237C93"
-    );
-    const fileToUpload: ShadowFile = {
-    name: "mytext.txt",
+        "https://rpc.helius.xyz/?api-key=ea657f44-c42a-4fc7-8829-f500359683a2", "confirmed"
+    );    const wallet = new anchor.Wallet(keypair);
+    const drive = await new ShdwDrive(connection, wallet).init(); 
+    const accts = await drive.getStorageAccounts("v2");
+    const fileBuff = fs.readFileSync("./qudo.gif");
+    let acctPubKey = new anchor.web3.PublicKey(accts[0].publicKey);
+    const fileToUpload: typeof ShadowFile = {
+    name: "qudo.gif",
     file: fileBuff,
     };
     const uploadFile = await drive.uploadFile(acctPubKey, fileToUpload);
     console.log(uploadFile);
+
 }
-
 main();
-
-//Upload Multiple Files
-//This is a nearly identical implementation to uploadFile, except that it requires a FileList or array of ShadowFiles and an optional concurrency parameter.
